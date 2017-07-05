dca149c504b44d0200bafcb4f41e9fe22ec972bf
Vasily Guzov <kinev1979@gmail.com>
2017-07-05 15:27:23 +0300

    fix view logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './Logo.css';
-import ReactVivus from '../ReactVivus'
 
 import SvgLogo from './vg-logo.svg'
 
 
 const Logo = (props) => {
     let {} = props;
-    return <ReactVivus name="Logo_VasilyGuzov" svg='./vg-logo.svg' color="red" />
+    return <SvgLogo />;
 };
 
 Logo.propTypes = {
