c658059419570ea6234c4e2f08b898e76d3e5171
Vasily Guzov <kinev1979@gmail.com>
2017-09-28 17:14:47 +0300

    created HeaderWithImg component

---
+++ src/layouts/stylegide.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
+
+import './index.css';
+import './fonts/stylegide.css';
+import './stylegide.css';
+import './grid/col.css';
+import './grid/12cols.css';
+
+const TemplateWrapper = ({children}) => (
+  <div>
+    <Helmet
+      title="style gide"
+      meta={[
+        {name: 'description', content: 'style gide'},
+        {name: 'keywords', content: 'sample, something'},
+      ]}
+    />
+    {children()}
+  </div>
+);
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+};
+
+export default TemplateWrapper;
