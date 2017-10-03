8602fb80fee013e8d8ca34344b6cd7650dfe86ab
Vasily Guzov <kinev1979@gmail.com>
2017-10-03 16:24:43 +0300

    test win commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,13 +2,12 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const SecondPage = ({ data }) => {
-  let { title, description } = data.site.siteMetadata.page2;
+  let { title, description } = data.site.siteMetadata.page2
   return <div>
     <h1>{ title }</h1>
     <p>{ description }</p>
     <Link to="/">Go back to the homepage</Link>
   </div>
-
 }
 
 export const query = graphql`
