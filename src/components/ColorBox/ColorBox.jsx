787218c7d8f3696347dc9d7ad6e628df55f91a5e
Vasily Guzov <kinev1979@gmail.com>
2017-10-03 19:04:40 +0300

    display ColorBox array

--- src/components/ColorBox/ColorBox.jsx
+++ src/components/ColorBox/ColorBox.jsx
@@ -5,13 +5,17 @@ const ColorBox = ({ colorValue, name }) => {
   let styles = {
     box: {
       backgroundColor: `#${colorValue}`,
+    },
+    value: {
+      color: (name === 'Light' || name === 'Grey') ? 'rgb(50, 77, 91)' : '#fff',
+      opacity: (name === 'Light' || name === 'Grey') ? '.5' : '1',
     }
   }
   return (
     <div className="ColorBox" style={ styles.box }>
       <div className="ColorBox__info">
         <div className="ColorBox__name">{ name }</div>
-        <div className="ColorBox__value" >{ colorValue }</div>
+        <div className="ColorBox__value" style={styles.value} >{ colorValue }</div>
       </div>
       <div className="ColorBox__shades">
         <span className="ColorBox__shade ColorBox__shade--dark"></span>
--- src/pages/stylegide.jsx
+++ src/pages/stylegide.jsx
@@ -3,16 +3,30 @@ import React from 'react'
 import HeaderWithImg from '../components/HeaderWithImg'
 import ColorBox from '../components/ColorBox'
 
+const COLORS = [
+  { name: 'Red', value: 'E2574C' },
+  { name: 'Green', value: '3db39e' },
+  { name: 'Blue', value: '2ea9d3' },
+  { name: 'Yellow', value: 'E2574C' },
+  { name: 'Light', value: 'f2f1ef' },
+  { name: 'Grey', value: 'd5d5d5' },
+  { name: 'Deep', value: '4681A0' },
+  { name: 'Dark', value: '324d5b' },
+]
+
 const StyleGide = () => {
+
   return (
     <div>
       <HeaderWithImg />
-      <div className="section group">
+      <div className="section group" style={{ maxWidth: '940px', margin: '0 auto' }}>
         <div className="col span_12_of_12">
-          <h1 style={{textAlign: 'center', fontSize: '72px', padding: '50px 0'}}>Basic Elements</h1>
+          <h1 style={{ textAlign: 'center', fontSize: '72px', padding: '50px 0' }}>Basic Elements</h1>
         </div>
-        <div className="col span_12_of_12">
-          <ColorBox colorValue="E2574C" name="Red" />
+        <div className="col span_12_of_12" style={{ display: 'flex', justifyContent: 'space-between' }}>
+          {
+            COLORS.map(({ name, value }, i) => <ColorBox key={Date.now() + `-${i}`} colorValue={value} name={name} />)
+          }
         </div>
       </div>
     </div>
