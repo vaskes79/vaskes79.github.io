8496d58810c06a2112283a6ef0a68f6f820695a0
Vasily Guzov <kinev1979@gmail.com>
2017-10-04 15:29:47 +0300

    created the Button component

--- src/pages/stylegide.jsx
+++ src/pages/stylegide.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 //import Link from 'gatsby-link'
 import HeaderWithImg from '../components/HeaderWithImg'
 import ColorBox from '../components/ColorBox'
+import Button from '../components/Button'
 
 const COLORS = [
   { name: 'Red', value: 'E2574C' },
@@ -27,6 +28,27 @@ const StyleGide = () => {
           {
             COLORS.map(({ name, value }, i) => <ColorBox key={Date.now() + `-${i}`} colorValue={value} name={name} />)
           }
+        </div>
+        <div className="col span_12_of_12" style={{ padding: '50px 0'}}>
+          <hr style={{height: 4}}/>
+        </div>
+        <div className="col span_12_of_12" style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button size='big' text='Hello' bgColor='green'/>
+          <Button size='big' text='Hello' bgColor='green'roundSize='0.188rem'/>
+          <Button size='big' text='Hello' bgColor='green'roundSize='0.5rem'/>
+          <Button size='big' text='Hello' bgColor='green'roundSize='1.688rem'/>
+        </div>
+        <div className="col span_12_of_12" style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button text='Hello' bgColor='red' />
+          <Button text='Hello' bgColor='red' roundSize='3px' />
+          <Button text='Hello' bgColor='red' roundSize='6px' />
+          <Button text='Hello' bgColor='red' roundSize='23px' />
+        </div>
+        <div className="col span_12_of_12" style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button size='small' text='Hello' bgColor='blue' />
+          <Button size='small' text='Hello' bgColor='blue' roundSize='3px' />
+          <Button size='small' text='Hello' bgColor='blue' roundSize='5px' />
+          <Button size='small' text='Hello' bgColor='blue' roundSize='20px' />
         </div>
       </div>
     </div>
