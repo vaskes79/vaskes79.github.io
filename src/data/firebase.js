f1c8e0c800261e16d4d5119da6cbd91381f5e289
Vasily Guzov <kinev1979@gmail.com>
2018-03-19 21:07:12 +0300

    setup environments variables

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,5 +1,22 @@
 import * as firebase from 'firebase'
-import CONFIG from './firebase-env'
+
+const {
+  VPORT_API_KEY,
+  VPORT_AUTH_DOMAIN,
+  VPORT_DATABASE_URL,
+  VPORT_PROJECT_ID,
+  VPORT_STORAGE_BUCKET,
+  VPORT_MESSAGING_SENDERID
+} = process.env;
+
+var CONFIG = {
+  apiKey: VPORT_API_KEY,
+  authDomain: VPORT_AUTH_DOMAIN,
+  databaseURL: VPORT_DATABASE_URL,
+  projectId: VPORT_PROJECT_ID,
+  storageBucket: VPORT_STORAGE_BUCKET,
+  messagingSenderId: VPORT_MESSAGING_SENDERID,
+}
 
 firebase.initializeApp(CONFIG)
 
