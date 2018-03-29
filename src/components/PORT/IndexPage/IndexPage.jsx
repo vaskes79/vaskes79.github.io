bcf6fe8e913b8e632ba2f98b2b9e65aa414d2e85
Vasily Guzov <kinev1979@gmail.com>
2018-03-29 21:44:44 +0300

    refactoring IndexPage component

--- src/components/PORT/IndexPage/IndexPage.jsx
+++ src/components/PORT/IndexPage/IndexPage.jsx
@@ -1,71 +1,112 @@
+// for dev
+import {DEMO_FOOTER_DATA, DEMO_HEADER_DATA, DEMO_RESUME_DATA, DEMO_FILTERS, DEMO_PORTFOLIO_ITEMS} from '../../../data'
+// end dev
 import React, {Component} from 'react'
 import {uniqueId} from 'lodash'
-
-import '../Themes'
-
+// import db modules
+// import {DB as db} from '../../../data/firebase'
 // import compones
-
 import Preloader from '../Preloader'
 import Resume from '../Resume'
 import {PortfolioArticle} from '../PortfolioItem'
 import Map from '../Map'
-
 import Header from '../Header'
 import Main from '../Main'
 import Footer from '../Footer'
 
+import '../Themes'
 import './IndexPage.css'
 
-// import db modules
-import {DB as db} from '../../../data/firebase'
-
-// for dev
-import DEMO_FOOTER_DATA, {map} from '../../../data/demo_footer_data.json'
-import DEMO_HEADER_DATA from '../../../data/demo_header_data.json'
-import DEMO_RESUME_DATA from '../../../data/demo_data_resume.json'
-import DEMO_FILTERS from '../../../data/demo_filter_portfolio_items.json'
-// don't forget reformate for portfolioItems shape this date
-import DEMO_PORTFOLIO_ITEMS from '../../../data/demo_portfolio_items.json'
 
 class IndexPage extends Component {
   state = {
     article: false,
-    loading: false,
+    loading: true,
     openResume: false,
     openMap: false,
   }
 
+  componentWillMount() {
+    this.getData()
+  }
+
+  getData = () => {
+    // let portfolioItemsCheck = DEMO_PORTFOLIO_ITEMS.length && DEMO_PORTFOLIO_ITEMS.length > 0;
+    // let filtersCheck = DEMO_FILTERS.length && DEMO_FILTERS.length > 0;
+    // let footerCheck = DEMO_FOOTER_DATA.contact && DEMO_FOOTER_DATA.address && DEMO_FOOTER_DATA.map;
+    // let resumeCheck = DEMO_RESUME_DATA.length && DEMO_RESUME_DATA.length > 4;
+    // let headerCheck = DEMO_HEADER_DATA.textIntro && DEMO_HEADER_DATA.title && DEMO_HEADER_DATA.subTutle;
+    // let loaded = portfolioItemsCheck && filtersCheck && footerCheck && resumeCheck && headerCheck;
+    let loaded = true;
+
+    if (loaded) {
+      this.setState({
+        portfolioItems: DEMO_PORTFOLIO_ITEMS,
+        filters: DEMO_FILTERS,
+        resumeData: DEMO_RESUME_DATA,
+        headerData: DEMO_HEADER_DATA,
+        footerData: DEMO_FOOTER_DATA,
+        mapData: DEMO_FOOTER_DATA.map,
+        loading: false
+      })
+    } else {
+      this.setState({
+        loading: true,
+      })
+    }
+
+  }
+
   handleOpenArticle = (articleId) => {
     console.log(`article id: ${articleId}`);
+    let article = DEMO_PORTFOLIO_ITEMS.filter( item => item._id === articleId)[0].content;
+    this.setState({article});
   }
 
   render () {
-    let {article, loading, openResume, openMap} = this.state;
+    let {article, loading, openResume, openMap, portfolioItems, filters, resumeData, headerData, footerData, mapData} = this.state;
+    let MAIN_PROPS = {
+      filters,
+      portfolioItems,
+      handleOpenArticle: this.handleOpenArticle,
+      handleFilter: () => console.log(`handleFilter on IndexPage is working`)
+    }
+    const HEADE_PROPS = {
+      ...headerData,
+      handleOpenResume: () => console.log('handle open resume is working on header')
+    }
+    const RESUME_PROPS = {
+      content: resumeData,
+      isOpen: openResume,
+      closeHandler: () => console.log('close handler resume is workign on resume')
+    }
+    const MAP_PROPS = {
+      ...mapData,
+      isOpen: openMap,
+      handleMap: () => console.log('handleMap on indexPage is working')
+    }
+    const FOOTER_DATA = { ...footerData }
     return (
       <div className="IndexPage">
-        {loading ? <Preloader /> : ''}
-        {openResume ? <Resume content={DEMO_RESUME_DATA} closeHandler={() => console.log('close handler resume is workign on resume')} isOpen={true}/> : '' }
-
-        {openMap ? <Map {...map} isOpen={true} handleMap={() => console.log("handleMap is working")} /> : ''}
-        {article ? <PortfolioArticle {...article} /> : '' }
+        {loading    ? <Preloader /> : ''}
+        {openResume ? <Resume {...RESUME_PROPS} /> : '' }
+        {openMap    ? <Map {...MAP_PROPS} /> : ''}
+        {article    ? <PortfolioArticle {...article} /> : '' }
 
         {
           loading || openResume || openMap || article ?
             null
           :
             <div className="IndexPage__wrap">
-              <Header {...DEMO_HEADER_DATA} handleOpenResume={() => console.log('handle open resume is working on header')}/>
-              <Main
-                filters={DEMO_FILTERS}
-                portfolioItems={DEMO_PORTFOLIO_ITEMS}
-                handleOpenArticle={this.handleOpenArticle}
-                handleFilter={() => console.log(`handleFilter on IndexPage is working`)}/>
-              <Footer {...DEMO_FOOTER_DATA}/>
+              <Header {...HEADE_PROPS} />
+              <Main {...MAIN_PROPS} />
+              <Footer {...FOOTER_DATA}/>
             </div>
         }
       </div>
     )
   }
 }
+
 
 export default IndexPage
---
+++ src/data/index.js
@@ -0,0 +1,13 @@
+import DEMO_FOOTER_DATA, {map} from './demo_footer_data.json'
+import DEMO_HEADER_DATA from './demo_header_data.json'
+import DEMO_RESUME_DATA from './demo_data_resume.json'
+import DEMO_FILTERS from './demo_filter_portfolio_items.json'
+import DEMO_PORTFOLIO_ITEMS from './demo_portfolio_items.json'
+
+export {
+  DEMO_FOOTER_DATA,
+  DEMO_HEADER_DATA,
+  DEMO_RESUME_DATA,
+  DEMO_FILTERS,
+  DEMO_PORTFOLIO_ITEMS,
+}
